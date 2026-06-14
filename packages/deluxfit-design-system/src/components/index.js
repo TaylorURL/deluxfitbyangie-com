23b653e6c295711db2d520fac8dea2dad6dee128
Sunday <trenton.taylor.email@gmail.com>
2026-06-14 16:16:23 -0500

    sunday: Write packages/deluxfit-design-system/src/components/index.js

---
+++ packages/deluxfit-design-system/src/components/index.js
@@ -0,0 +1,23 @@
+// Actions
+export { Button, buttonVariants } from './Button'
+
+// Data display
+export { Badge, badgeVariants } from './Badge'
+export { Card, cardVariants } from './Card'
+export { StatBlock } from './StatBlock'
+export { Testimonial } from './Testimonial'
+export { PricingCard } from './PricingCard'
+
+// Disclosure
+export { Accordion, FaqItem } from './Accordion'
+
+// Forms
+export { Input, inputVariants } from './Input'
+export { Field } from './Field'
+
+// Marketing primitives
+export { SectionEyebrow } from './SectionEyebrow'
+export { Marquee } from './Marquee'
+
+// Motion
+export { Reveal, revealVariants, staggerContainer } from './Reveal'
