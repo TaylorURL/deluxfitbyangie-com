b46fe632594b898b80b4f2d0c5427210386fd76a
Sunday <trenton.taylor.email@gmail.com>
2026-06-17 22:07:01 -0500

    sunday: Edit src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import './index.css'
 import App from './App'
 import { LanguageProvider } from './i18n'
 import AuthProvider from './auth/AuthProvider'
+import { SundayAnalyticsProvider } from './lib/sunday-analyzer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LanguageProvider>
-      <AuthProvider>
-        <MotionConfig reducedMotion="user">
-          <App />
-        </MotionConfig>
-      </AuthProvider>
-    </LanguageProvider>
+    <SundayAnalyticsProvider siteKey="sa_e65b4412338d559454807a3c2fd2b362">
+      <LanguageProvider>
+        <AuthProvider>
+          <MotionConfig reducedMotion="user">
+            <App />
+          </MotionConfig>
+        </AuthProvider>
+      </LanguageProvider>
+    </SundayAnalyticsProvider>
   </StrictMode>
 )
